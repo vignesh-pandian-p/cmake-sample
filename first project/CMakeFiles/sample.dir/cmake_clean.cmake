a2229d6b24a59bddf6aa2687901a39281f3dfe4b
vignesh-pandian-p <vigneshpandian0710@gmail.com>
2024-12-08 11:05:15 +0530

    simple project files with basic cpp

---
+++ first project/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/src/sample.cpp.o"
+  "CMakeFiles/sample.dir/src/sample.cpp.o.d"
+  "sample"
+  "sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
